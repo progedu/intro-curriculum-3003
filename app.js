a2c73eb4200221a9f3cdcba6764b746ce6ed9c65
Soichiro-Yoshimura <soichiro_yoshimura@dwango.co.jp>
2015-12-08 18:10:18 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+(function () {
+	'use strict';
+	let memo = new Map();
+	memo.set(0, 0);
+	memo.set(1, 1);
+	function fib(n) {
+		if(memo.has(n)) {
+			return memo.get(n);
+		}
+		let value = fib(n - 1) + fib(n - 2);
+		memo.set(n, value);
+		return value;
+	}
+	let length = 40;
+	for (let i = 0; i < length; i++) {
+		console.log(fib(i));
+	}
+})();
