1d1061a09ba897378dd2db44aae0b64f89b345a4
Soichiro Yoshimura <yoshimura@soichiro.org>
2015-12-18 15:09:01 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,16 +3,16 @@
 	let memo = new Map();
 	memo.set(0, 0);
 	memo.set(1, 1);
-	function fib(n) {
+	function trib(n) {
 		if(memo.has(n)) {
 			return memo.get(n);
 		}
-		let value = fib(n - 1) + fib(n - 2);
+		let value = trib(n - 1) + trib(n - 2);
 		memo.set(n, value);
 		return value;
 	}
 	let length = 40;
 	for (let i = 0; i < length; i++) {
-		console.log(fib(i));
+		console.log(trib(i));
 	}
 })();
