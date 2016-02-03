0971fe42269de0c5d15aeb82edbc3b4d6fc5af9c
Shutaro Takimoto <shutaro_takimoto@dwango.co.jp>
2016-02-03 10:38:33 +0000

    トリボナッチ数列の計算を実装

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 (function () {
 	'use strict';
 	let memo = new Map();
-	memo.set(0, 0);
-	memo.set(1, 1);
+	memo.set(0, 0); // F(0) = 0
+	memo.set(1, 0); // F(1) = 0
+    memo.set(2, 1); // F(2) = 1
 	function trib(n) {
-		if(memo.has(n)) {
+		if (memo.has(n)) {
 			return memo.get(n);
 		}
-		let value = trib(n - 1) + trib(n - 2);
+        // F(n) = F(n - 1) + F(n - 2) + F(n -3) ただし n > 2
+		let value = trib(n - 1) + trib(n - 2) + trib(n - 3);
 		memo.set(n, value);
 		return value;
 	}
