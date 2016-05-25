0a1a7b15a00e1827de9ba57999edd9e4b6fba365
Yuya Takashina <yuya_takashina@dwango.co.jp>
2016-05-25 16:10:05 +0900

    let -> const

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 (function () {
 	'use strict';
-	let memo = new Map();
+	const memo = new Map();
 	memo.set(0, 0);
 	memo.set(1, 1);
 	function trib(n) {
 		if(memo.has(n)) {
 			return memo.get(n);
 		}
-		let value = trib(n - 1) + trib(n - 2);
+		const value = trib(n - 1) + trib(n - 2);
 		memo.set(n, value);
 		return value;
 	}
-	let length = 40;
+	const length = 40;
 	for (let i = 0; i < length; i++) {
 		console.log(trib(i));
 	}
