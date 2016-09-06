79809e8413c002d0a8c2ee13a5d0564104ff19b6
mizut <mztns.net@gmail.com>
2016-09-06 15:49:49 +0000

    テストを追加

--- app.js
+++ app.js
@@ -15,3 +15,13 @@ const length = 40;
 for (let i = 0; i < length; i++) {
     console.log(trib(i));
 }
+
+console.assert(
+    trib(10) === 81,
+    '10番目のトリボナッチ数列を帰す'
+);
+
+console.assert(
+    trib(1000) === 8.155077059490629e+263,
+    '1000番目のトリボナッチ数列を帰す'
+);
