14135b885b3526c441c96fdc5d5df3b295f49304
murabito <yuusuke_17n1100165@nnn.ed.jp>
2017-04-06 18:53:17 +0000

    フィボナッチ数列のコードからトリボナッチ数列のコードに変更

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 'use strict';
+//フィボナッチ数列のコードからトリボナッチ数列のコードに変更
+/*トリボナッチ数列の定義は、　︎　︎
+    F0 = 0
+    F1 = 0
+    F2 = 1
+    Fn+3 = Fn + Fn+1 + Fn+2 (n ≧ 0)
+です。*/
 const memo = new Map();
-memo.set(0, 0);
-memo.set(1, 1);
-function trib(n) {
-    if (memo.has(n)) {
+memo.set(0,0);
+memo.set(1,0);
+memo.set(2,1);
+function trib(n){
+    if (memo.has(n)){
         return memo.get(n);
     }
-    const value = trib(n - 1) + trib(n - 2);
-    memo.set(n, value);
+    const value = trib(n - 1) + trib(n - 2) + trib(n - 3);
+    memo.set(n,value);
     return value;
 }
 const length = 40;
-for (let i = 0; i <= length; i++) {
-    console.log(trib(i));
-}
+    for (let i = 0; i <= length; i++){
+        console.log(trib(i));
+    }
