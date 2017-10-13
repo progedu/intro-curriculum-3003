7b67bbbe30a872384ea35ad62be8363f430b2be9
kokokocococo555 <kokokocococo555@gmail.com>
2017-10-13 04:19:31 +0000

    O(n)のトリボナッチ関数の実装

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 'use strict';
 const memo = new Map();
-memo.set(0, 0);
-memo.set(1, 1);
-function trib(n) {
-    if (memo.has(n)) {
+memo.set(0,0);
+memo.set(1,0);
+memo.set(2,1);
+function tb(n){
+    if(memo.has(n)){
         return memo.get(n);
     }
-    const value = trib(n - 1) + trib(n - 2);
+    const value = tb(n-1) + tb(n-2) + tb(n-3);
     memo.set(n, value);
-    return value;
+    return value; 
 }
 const length = 40;
-for (let i = 0; i <= length; i++) {
-    console.log(trib(i));
+for(let i =0; i <= length; i++){
+    console.log(tb(i));
 }
