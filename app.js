ad9f6a3846b76a11cab3439a7fb6276b3e9a7df2
yamamotoshu1127 <yamamotoshu1127@gmail.com>
2017-11-13 11:21:01 +0000

    コメントの追記

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 'use strict';
+// 計算結果を保持するキャッシュ
 const memo = new Map();
 memo.set(0, 0);
 memo.set(1, 0);
 memo.set(2, 1);
+
+/**
+ * トリボナッチ数列の解を計算
+ * 
+ * @param {number}
+ */
 function trib(n) {
+    // 計算済みである場合は値を取得
     if (memo.has(n)) {
         return memo.get(n);
     }
@@ -11,6 +19,7 @@ function trib(n) {
     memo.set(n, value);
     return value;
 }
+
 const length = 40;
 for (let i = 0; i <= length; i++) {
     console.log(trib(i));
