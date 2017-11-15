27bcb1aec15e53a87ce1d0c4e2a20e9ba3657719
yamamotoshu1127 <yamamotoshu1127@gmail.com>
2017-11-15 15:36:04 +0000

    復習用の実装を追加

--- app.js
+++ app.js
@@ -5,6 +5,25 @@ memo.set(0, 0);
 memo.set(1, 0);
 memo.set(2, 1);
 
+const map = new Map();
+map.set(0, 0);
+map.set(1, 1);
+
+/**
+ * フィボナッチ数列の解を計算
+ * 
+ * @param {number}
+ */
+function fib(n) {
+    if (map.has(n)) {
+        return map.get(n);
+    }
+
+    const value = fib(n - 1) + fib(n - 2);
+    map.set(n, value);
+    return value;
+}
+
 /**
  * トリボナッチ数列の解を計算
  * 
@@ -21,6 +40,11 @@ function trib(n) {
 }
 
 const length = 40;
+console.log('フィボナッチ数列');
+for (let i = 0; i <= length; i++) {
+    console.log(fib(i));
+}
+console.log('トリボナッチ数列');
 for (let i = 0; i <= length; i++) {
     console.log(trib(i));
 }
