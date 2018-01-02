d86c1c7cb4677f944de859962b431c54d5509543
Makoto Ishida <makoto_16n1200102@nnn.ed.jp>
2018-01-02 19:22:45 +0000

    トリボナッチ数列を実装

---
+++ src/app.js
@@ -0,0 +1,15 @@
+'use strict'
+const memo = new Map()
+memo.set(0, 0)
+memo.set(1, 0)
+memo.set(2, 1)
+function trib(n) {
+  if (!memo.has(n)) {
+    memo.set(n, trib(n - 1) + trib(n - 2) + trib(n - 3))
+  }
+  return memo.get(n)
+}
+const length = 40
+for (let i = 0; i <= length; i++) {
+    console.log(trib(i));
+}
