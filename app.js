1604bfdf44543c3b1caa7438c583b23b175057f7
popotanu <d.kaneko.d@gmail.com>
2018-01-24 00:05:55 +0900

    トリボナッチ数列の実装

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 'use strict';
 const memo = new Map();
 memo.set(0, 0);
-memo.set(1, 1);
+memo.set(1, 0);
+memo.set(2, 1);
 function trib(n) {
+    if (n < 0) {
+        throw new Error('引数には0または自然数を指定してね');
+    }
     if (memo.has(n)) {
         return memo.get(n);
     }
-    const value = trib(n - 1) + trib(n - 2);
+    const value = trib(n - 1) + trib(n - 2) + trib(n - 3);
     memo.set(n, value);
     return value;
 }
