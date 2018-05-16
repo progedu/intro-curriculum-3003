2a216c4413b1f5bf16432d8132612565824ea74c
danoguchi <39083076+danoguchi@users.noreply.github.com>
2018-05-16 14:23:05 +0900

    typo修正
    fib関数名をtribに変更

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ memo.set(0, 0);
 memo.set(1, 0);
 memo.set(2, 1);
 
-function fib(n){
+function trib(n){
     if(memo.has(n)){
         return memo.get(n);
     }
 
-    const value = fib(n - 1) + fib(n - 2) + fib(n - 3);
+    const value = trib(n - 1) + trib(n - 2) + trib(n - 3);
     memo.set(n, value)
     return value;
 
@@ -17,5 +17,5 @@ function fib(n){
 
 const length = 40;
 for (let i = 0; i <= length; i++){
-    console.log(fib(i));
+    console.log(trib(i));
 }
