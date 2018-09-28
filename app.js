293415e8794ea01d9763af2a52c9182486aa4cef
ponzu66503624 <stskm18@gmail.com>
2018-09-28 13:52:10 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 'use strict';
 const memo = new Map();
-memo.set(0, 0);
-memo.set(1, 1);
-function trib(n) {
-    if (memo.has(n)) {
-        return memo.get(n);
+//連想配列を定義
+memo.set(0,0)//
+memo.set(1,0)//
+memo.set(2,1)//
+function trib(Fn){
+    if(memo.has(Fn)){
+        return memo.get(Fn);
     }
-    const value = trib(n - 1) + trib(n - 2);
-    memo.set(n, value);
-    return value;
+    const value = trib(Fn - 1) + trib(Fn - 2) + trib(Fn - 3);
+    memo.set(Fn,value);
+    return value 
 }
-const length = 40;
-for (let i = 0; i <= length; i++) {
+const lenght = 40;
+for(let i = 0; i <= lenght; i++){
     console.log(trib(i));
 }
