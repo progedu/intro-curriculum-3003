56235746819d34ee733db780b4db7c1eaf29fdda
toshi <toshi3arai333@gmail.com>
2019-05-24 15:22:24 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 'use strict';
 const memo = new Map();
 memo.set(0, 0);
-memo.set(1, 1);
+memo.set(1, 0);
+memo.set(2, 1);
+//ここの意味が分かってない。
+//0番目は0、1番目は0、2番目は1まではここで定義する
 function trib(n) {
     if (memo.has(n)) {
         return memo.get(n);
     }
-    const value = trib(n - 1) + trib(n - 2);
+    const value = trib(n - 1) + trib(n - 2) + trib(n-3);
+    //その後は 1 つ前と 2 つ前と 3 つ前の値を足したものとなります。
+
     memo.set(n, value);
     return value;
 }
