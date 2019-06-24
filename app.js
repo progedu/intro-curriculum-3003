6381abef469828f902e1f63447f4318ce006fdef
touei10 <tokoroten122121@gmail.com>
2019-06-25 07:56:02 +0900

    Tribonacciのmemo化によるalgoの改善

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 'use strict';
+// tribonacci number
+
 const memo = new Map();
 memo.set(0, 0);
-memo.set(1, 1);
+memo.set(1, 0);
+memo.set(2, 1);
+
 function trib(n) {
     if (memo.has(n)) {
         return memo.get(n);
+    } else {
+        const value = trib(n - 1) + trib(n - 2) + trib(n-3);
+        memo.set(n, value);
+        return value;
     }
-    const value = trib(n - 1) + trib(n - 2);
-    memo.set(n, value);
-    return value;
 }
 const length = 40;
 for (let i = 0; i <= length; i++) {
-    console.log(trib(i));
+    console.log("Tn=" + i + ", ans=" + trib(i));
 }
