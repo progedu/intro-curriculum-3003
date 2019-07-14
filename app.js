d20df645192e7a1710607cb2d6342bc3c289b74a
あさづけ <quusan3@gmail.com>
2019-07-14 09:52:09 +0900

    メモ化してトリボナッチ数列を出力するコード

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 'use strict';
 const memo = new Map();
 memo.set(0, 0);
-memo.set(1, 1);
+memo.set(1, 0);
+memo.set(2, 1);
 function trib(n) {
     if (memo.has(n)) {
         return memo.get(n);
     }
-    const value = trib(n - 1) + trib(n - 2);
+    const value = trib(n - 1) + trib(n - 2) + trib(n - 3);
     memo.set(n, value);
     return value;
 }
-const length = 40;
+const length = process.argv[2] || 0;
+
 for (let i = 0; i <= length; i++) {
     console.log(trib(i));
 }
+
+const assert = require('assert');
+assert.equal(trib(1), 0, `1のトリボナッチ数は0ですが、実際は${trib(1) }でした`);
+assert.equal(trib(2), 1, `2のトリボナッチ数は1ですが、実際は${trib(2) }でした`);
+assert.equal(trib(3), 1, `3のトリボナッチ数は1ですが、実際は${trib(3) }でした`);
+assert.equal(trib(10), 81, `10のトリボナッチ数は81ですが、実際は${trib(10) }でした`);
+console.log('すべてのテストを通過しました');
