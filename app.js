3c41cae9d5c51b93c767ff6a7904599441994c34
TOUEi10 <52106355+touei10@users.noreply.github.com>
2019-08-29 22:40:08 +0900

    トリボナッチ数列に変更

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 'use strict';
 // tribonacci number
-
 const memo = new Map();
 memo.set(0, 0);
 memo.set(1, 0);
 memo.set(2, 1);
 
+function trib(n) {
+    if (memo.has(n)) {
+        return memo.get(n);
+    }
+    const value = trib(n - 1) + trib(n - 2) + trib(n - 3);
+    memo.set(n, value);
+    return value;
+}
+
+const length = 40;
+for (let i = 0; i <= length; i++) {
+    console.log(i + `番目:` + trib(i));
+}
+
+/*
 function trib(n) {
     if (memo.has(n)) {
         return memo.get(n);
@@ -19,3 +33,4 @@ const length = 40;
 for (let i = 0; i <= length; i++) {
     console.log("Tn=" + i + ", ans=" + trib(i));
 }
+*/
