a1ae8b1db1e77a1ff9af1ae040cef70831537597
AKR1958 <mathtikoa+2019pg@gmail.com>
2019-09-07 16:24:02 +0900

    トリボナッチ数列の実装

--- app.js
+++ app.js
@@ -2,13 +2,13 @@
 const memo = new Map();
 memo.set(0, 0);
 memo.set(1, 1);
+memo.set(2, 1);
 function trib(n) {
     if (memo.has(n)) {
         return memo.get(n);
     }
-    const value = trib(n - 1) + trib(n - 2);
-    memo.set(n, value);
-    return value;
+    memo.set(n,trib(n - 1) + trib(n - 2) + trib(n - 3))
+    return memo.get(n);
 }
 const length = 40;
 for (let i = 0; i <= length; i++) {
