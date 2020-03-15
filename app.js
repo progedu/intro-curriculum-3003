a57b848f8f0a9b857003529be38194b1c0fc8b10
twugo <hm17031@student.miyazaki-u.ac.jp>
2020-03-15 22:06:41 +0900

    Update:メモ化処理による高速化

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
 'use strict';
-const memo = new Map();
-memo.set(0, 0);
-memo.set(1, 1);
-function trib(n) {
-    if (memo.has(n)) {
-        return memo.get(n);
+
+/**
+ * トリボナッチ数処理のためのクラス
+ */
+class Trib{
+    /**
+     * メモ化用変数の用意
+     */
+    constructor(){
+        this.memo = new Map();
+        this.memo.set(0, 0);
+        this.memo.set(1, 0);
+        this.memo.set(2, 1);
     }
-    const value = trib(n - 1) + trib(n - 2);
-    memo.set(n, value);
-    return value;
+    
+    /**
+     * トリボナッチ数列の n 番目の値を返す関数。
+     * @param {Number} n    数列の何番目か
+     * @return {Number}    数列の n 番目の値
+     */
+    trib(n){
+        if(this.memo.has(n)){
+            return this.memo.get(n);
+        }
+        const value = this.trib(n-1) + this.trib(n-2) + this.trib(n-3);
+        this.memo.set(n, value);
+        return value;
+    }
 }
+
 const length = 40;
+var tribonacchi = new Trib();
 for (let i = 0; i <= length; i++) {
-    console.log(trib(i));
+    console.log(tribonacchi.trib(i));
 }
