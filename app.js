3f7adb9fa3323bca11e32da14b7f5e8cc086058c
parsely1231 <52167040+parsely1231@users.noreply.github.com>
2020-03-21 15:29:52 +0900

    trib関数　非再帰で実装
    再帰関数を使わずに実装してみました。

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 'use strict';
-const memo = new Map();
-memo.set(0, 0);
-memo.set(1, 1);
-function trib(n) {
-    if (memo.has(n)) {
-        return memo.get(n);
+
+function trib (n) {
+    if (n === 0 | n === 1) {
+        return 0;
     }
-    const value = trib(n - 1) + trib(n - 2);
-    memo.set(n, value);
-    return value;
+    let prepre = 0;
+    let pre = 0;
+    let current = 1;
+    for (let i = 2; i < n; i++) {
+        let next = current + pre + prepre;
+        prepre = pre;
+        pre = current;
+        current = next;
+    }
+    return current;
 }
+
 const length = 40;
 for (let i = 0; i <= length; i++) {
     console.log(trib(i));
