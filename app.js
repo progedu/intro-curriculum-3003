d1ef81f50642189f6efd1fc404bd27c54ee81f2f
tutimin <tutimikado0203@gmail.com>
2020-03-22 22:55:32 +0900

    トリボナッチ数列のコード変更

--- app.js
+++ app.js
@@ -9,15 +9,15 @@ const memo = new Map();
 memo.set(0, 0);
 memo.set(1, 0);
 memo.set(2, 1);
-function factorial(n) {
+function trib(n) {
     if (memo.has(n)) {
         return memo.get(n);
     }
-    const value = factorial(n - 1) + factorial(n - 2) + factorial(n - 3);
+    const value = trib(n - 1) + trib(n - 2) + trib(n - 3);
     memo.set(n, value);
     return value;
 }
 const length = 40;
 for (let i = 0; i <= length; i++) {
-    console.log(factorial(i));
+    console.log(trib(i));
 }
