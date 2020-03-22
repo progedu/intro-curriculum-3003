abf05387ff26e3d1887866717ca49e6a013af87b
tutimin <tutimikado0203@gmail.com>
2020-03-22 22:54:20 +0900

    トリボナッチ数列のコード変更

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 'use strict';
+/**
+* 与えられた自然数の階乗を返す
+* 階乗とは、1からその与えられた自然数までの数をすべてかけたものです
+* @param {Number} n
+* @returns {Number}
+*/
 const memo = new Map();
 memo.set(0, 0);
-memo.set(1, 1);
-function trib(n) {
+memo.set(1, 0);
+memo.set(2, 1);
+function factorial(n) {
     if (memo.has(n)) {
         return memo.get(n);
     }
-    const value = trib(n - 1) + trib(n - 2);
+    const value = factorial(n - 1) + factorial(n - 2) + factorial(n - 3);
     memo.set(n, value);
     return value;
 }
 const length = 40;
 for (let i = 0; i <= length; i++) {
-    console.log(trib(i));
+    console.log(factorial(i));
 }
