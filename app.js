179e9677cc242f5522b08f7bfcfb0cb41710ed6c
queue44int <masa320@tulip.ocn.ne.jp>
2020-04-15 16:39:25 +0900

    implement

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 'use strict';
-const memo = new Map();
-memo.set(0, 0);
-memo.set(1, 1);
-function trib(n) {
-    if (memo.has(n)) {
-        return memo.get(n);
+function TribonachCalculator() {
+  this._memo = new Map();
+  this._memo.set(0, 0);
+  this._memo.set(1, 0);
+  this._memo.set(2, 1);
+
+  this.calculate = (n) => {
+    if (this._memo.has(n)) {
+      return this._memo.get(n);
+    } else {
+      let res = this.calculate(n-3) + this.calculate(n-2) + this.calculate(n-1);
+      this._memo.set(n, res);
+      return res;
     }
-    const value = trib(n - 1) + trib(n - 2);
-    memo.set(n, value);
-    return value;
+  }
+
+  this.calculate_and_print = (n) => {
+    this.calculate(n);
+    let fun = (value,key,map) => {
+        process.stdout.write(value.toString());
+        if (key != n) process.stdout.write(", ");
+        else process.stdout.write("\n");
+    };
+    this._memo.forEach(fun);
+  }
 }
-const length = 40;
-for (let i = 0; i <= length; i++) {
-    console.log(trib(i));
-}
+
+const number = process.argv[2] || 40;
+
+let calculator = new TribonachCalculator();
+calculator.calculate_and_print(number);
