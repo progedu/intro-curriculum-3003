62feb1573f3da39db46d0d7f6869102bcb64515f
junkow <blackcatstep1125@msn.com>
2020-04-16 15:49:52 +0900

    トリナボッチ数列の計算の実装

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 'use strict';
 const memo = new Map();
 memo.set(0, 0);
-memo.set(1, 1);
+memo.set(1, 0);
+memo.set(2, 1);
 function trib(n) {
+    if(n < 0) { return; }
     if (memo.has(n)) {
         return memo.get(n);
     }
-    const value = trib(n - 1) + trib(n - 2);
+    const value = trib(n - 1) + trib(n - 2) + trib(n - 3);
     memo.set(n, value);
     return value;
 }
@@ -14,3 +16,14 @@ const length = 40;
 for (let i = 0; i <= length; i++) {
     console.log(trib(i));
 }
+
+const assert = require('assert');
+const result = [0,0,1,1,2,4,7,13,24,44,81,149,274,504,927,1705,
+    3136,5768,10609,19513,35890,66012,121415,223317,
+    410744,755476,1389537,2555757,4700770,8646064,
+    15902591,29249425,53798080,98950096,181997601,
+    334745777,615693474,1132436852];
+for(let i = 0; i < result.length; i++) {
+    assert.equal(trib(i), result[i], `トリボナッチ数列の${i}番目は${result[i]}ですが、実際は${trib(i)}でした`);
+}
+console.log('全てのテストを通過しました');
