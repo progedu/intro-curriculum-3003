96fd725299a2dd4732cb7ccd025091aafc515318
applego <applegorillappa@gmail.com>
2020-04-20 15:48:39 +0900

    トリボナッチ数列

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 'use strict';
-const memo = new Map();
-memo.set(0, 0);
-memo.set(1, 1);
-function trib(n) {
-    if (memo.has(n)) {
-        return memo.get(n);
+const mapTri = new Map();
+mapTri.set(0, 0);
+mapTri.set(1, 0);
+mapTri.set(2, 1);
+
+function tri(n) {
+    if (mapTri.has(n)) {
+        return mapTri.get(n);
     }
-    const value = trib(n - 1) + trib(n - 2);
-    memo.set(n, value);
+    const value = tri(n - 3) + tri(n - 2) + tri(n - 1);
+    mapTri.set(n, value);
     return value;
 }
-const length = 40;
 for (let i = 0; i <= length; i++) {
-    console.log(trib(i));
+    console.log(i + ' ' + tri(i));
 }
