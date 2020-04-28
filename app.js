4b6591845f99fdb2a2df1cf8f254c7f5f846b109
K. Higashino <44574482+knt-tky-van@users.noreply.github.com>
2020-04-28 11:12:01 +0900

    modification

--- app.js
+++ app.js
@@ -2,15 +2,16 @@
 const memo = new Map();
 memo.set(0, 0);
 memo.set(1, 1);
-function trib(n) {
-    if (memo.has(n)) {
-        return memo.get(n);
-    }
-    const value = trib(n - 1) + trib(n - 2);
-    memo.set(n, value);
-    return value;
+memo.set(2, 1);
+function fib(n) {
+  if (memo.has(n)) {
+    return memo.get(n);
+  }
+  const value = fib(n - 1) + fib(n - 2)+ trib(n - 3);
+  memo.set(n, value);
+  return value;
 }
 const length = 40;
 for (let i = 0; i <= length; i++) {
-    console.log(trib(i));
+  console.log(fib(i));
 }
