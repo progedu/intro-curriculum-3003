e480f346dfec2a4e919f1e01737442d50973ef0c
n-hgc <gpxxakag@gmail.com>
2020-05-04 01:45:17 +0900

    メモ化アルゴリズムを利用して、トリボナッチ数列を作成

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 'use strict';
+
 const memo = new Map();
-memo.set(0, 0);
-memo.set(1, 1);
-function trib(n) {
-    if (memo.has(n)) {
-        return memo.get(n);
-    }
-    const value = trib(n - 1) + trib(n - 2);
-    memo.set(n, value);
-    return value;
+
+memo.set(0,0);
+memo.set(1,0);
+memo.set(2,1);
+
+function tib(n) {
+	if (memo.has(n)) {
+		return memo.get(n);
+	}
+	const value = tib(n-1) + tib(n-2) + tib(n-3);
+	memo.set(n, value);
+	return value;
 }
+
 const length = 40;
-for (let i = 0; i <= length; i++) {
-    console.log(trib(i));
+let i = 0;
+
+while (i <= length) {
+	console.log(tib(i));
+	i++;
 }
+
