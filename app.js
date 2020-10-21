a68f78ca230ea835cc1ed4b50d39f6f089fc5186
kokuyone <66826171+kokuyone@users.noreply.github.com>
2020-10-22 04:25:37 +0900

    トリボナッチ数列 練習問題

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 'use strict';
 const memo = new Map();
 memo.set(0, 0);
-memo.set(1, 1);
-function trib(n) {
-    if (memo.has(n)) {
-        return memo.get(n);
+memo.set(1, 0);
+memo.set(2, 1);
+
+function tli(Fn) {
+    if (memo.has(Fn)) {
+        return memo.get(Fn);
     }
-    const value = trib(n - 1) + trib(n - 2);
-    memo.set(n, value);
+    const value = tli(Fn - 1) + tli(Fn - 2) + tli(Fn - 3);
+    memo.set(Fn, value);
     return value;
 }
 const length = 40;
 for (let i = 0; i <= length; i++) {
-    console.log(trib(i));
+    console.log(tli(i));
 }
+
